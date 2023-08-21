2d8fcdbb2e505681ae21218c2f838674f33b76f0
Ruben Montes <montesgonzalezruben2@gmail.com>
2023-08-21 19:32:19 +0200

    Modificamos estidos de appBar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.blue,
+  Colors. greenAccent,
+  Colors.pink,
+  Colors.orange,
+  Colors.tealAccent
+];
+
+
+class AppTheme{
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+    }): assert(selectedColor >= 0, 'Selected color must be gratter than 0'),
+        assert(selectedColor < colorList.length, 'Selected color must be lower than ${colorList.length}');
+
+
+    ThemeData getTheme() => ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+      )
+    );
+
+
+
+}
