18428dc0b10d110dd86896d730f873f613ad53d6
Ruben Montes <montesgonzalezruben2@gmail.com>
2023-08-29 21:09:19 +0200

    Infinite Scroll comenzao

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -71,5 +71,12 @@ const appMenuItems = <MenuItem>[
     icon: Icons.accessibility_new_outlined
     ),
 
+    MenuItem(
+    title: 'Infinite Scroll y pull', 
+    subTitle: 'Listas infinitas y pull to refresh', 
+    link: '/infinite', 
+    icon: Icons.list_alt_rounded
+    ),    
+
 
 ];
--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -2,15 +2,45 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
 
   static const name = 'infinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+
+  List<int> imagesIds = [1,2,3,4,5];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: const AssetImage('assets/Images/jar-loading.gif'), 
+              image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300'));
+          },
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.exit_to_app),
+        onPressed: () => context.pop()),
+
+    );
   }
 }
